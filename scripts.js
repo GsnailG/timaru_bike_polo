4c298b0c6824a08c1e27b774e21fa5f385168b46
GSnailG <garethkearins@gmail.com>
2022-07-07 16:44:39 +1200

    first ever loading screen!

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.onload = function websiteload() {
+    document.getElementById("preload").style.animation = "fadeOut 1s forwards linear";
+    document.getElementById("preload").style.zIndex = "-30";
+}
+
