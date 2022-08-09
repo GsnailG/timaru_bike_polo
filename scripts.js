d591632666b367709fd1d6cd511f1437338c6df2
GSnailG <garethkearins@gmail.com>
2022-08-09 12:33:29 +1200

    idk i added geese?

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 window.onload = function websiteload() {
-    document.getElementById("preload").style.animation = "fadeOut 1s forwards linear";
+    document.getElementById("preload").style.animation = "fadeOut 1s forwards linear"; 
+    /*add a fade animation to the loading screen, when the website has finished loading */
     document.getElementById("preload").style.zIndex = "-30";
+    /*move the element behind the main website*/
 }
 
+/* do not use display none as the website will 
+not load the normal page behind the loading 
+screen, therfore making the loader useless */
